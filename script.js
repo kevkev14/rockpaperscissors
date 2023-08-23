3ed97285a05b5055816475a5ce8214408c2f1783
marv0659 <marv0659@stud.kea.dk>
2023-08-23 19:19:11 +0200

    bro ting og sager

--- script.js
+++ script.js
@@ -1,27 +1,116 @@
 const player1Text = document.querySelector("#player1Text");
 const player2Text = document.querySelector("#player2Text");
 const resultText = document.querySelector("#resultText");
-const choiceBtns = document.querySelectorAll(".choiceBtn");
+const choiceBtns = document.querySelectorAll(".buttons");
 
-document.querySelector(".rock").addEventListener("click", player1Turn);
+document.querySelector(".rock").addEventListener("click", rock);
+document.querySelector(".paper").addEventListener("click", paper);
+document.querySelector(".scissors").addEventListener("click", scissor);
+
 
 let player1;
 let player2;
 let result;
 
-choiceBtns.forEach((button) =>
+  /* choiceBtns.forEach((button) =>
   button.addEventListener("click", () => {
     player1 = button.textContent;
     player2Turn();
+    console.log("player2Turn")
     player1Text.textContent = `player1: ${player1}`;
     player2Text.textContent = `player2: ${player2}`;
     resultText.textContent = checkWinner();
   })
-);
+);  */
 
-function player1Turn() {
-  let player1 = "ROCK";
-  checkWinner();
+
+
+/* function checkWinner(player1, player2) {
+  if (player1 === player2) {
+    document.querySelector("#draw").classList.remove("hidden");
+    console.log("ass")
+    
+    
+  }
+   else if (player2 === "rock") {
+    return player1 === "paper" ? "You Win!" : "You Lose!";
+  }
+   else if (player2 === "paper") {
+  
+    return player1 === "scissors" ? "You Win!" : "You Lose!";
+  }
+   else if (player2 === "scissors") {
+    
+    return player1 === "rock" ? "You Win!" : "You Lose!";
+  }
+} */
+
+
+function checkWinner(player1, player2){
+  if (player1 === player2){
+    document.querySelector("#player1").classList.add("shake")
+    document.querySelector("#draw").classList.remove("hidden");
+  }
+  else if (player1 === "rock"){
+    if (player2 === "scissors"){
+    }
+    document.querySelector("#win").classList.remove("hidden");
+
+  }
+  else if (player1 === "scissors"){
+    if (player2 === "rock"){
+    }
+    document.querySelector("#lose").classList.remove("hidden");
+
+  }
+
+  else if (player1 === "scissors"){
+    if(player2 === "paper"){
+      document.querySelector("#win").classList.remove("hidden");
+
+    }
+  }
+  else if (player1 === "paper"){
+    if(player2 === "scissors"){
+      document.querySelector("#lose").classList.remove("hidden");
+
+    }
+  }
+
+  else if (player1 === "paper"){
+    if(player2 === "rock"){
+      document.querySelector("#win").classList.remove("hidden");
+
+    }
+  }
+  else if (player1 === "rock"){
+    if(player2 === "paper"){
+      document.querySelector("#lose").classList.remove("hidden");
+
+    }
+  }
+}
+
+
+function rock() {
+  let player1 = "rock";
+  checkWinner(player1, player2);
+  console.log("player1Turn", player1)
+  console.log(player2)
+}
+
+function paper() {
+  let player1 = "paper";
+  checkWinner(player1, player2);
+  console.log("player1Turn", player1)
+  console.log(player2)
+}
+
+function scissor() {
+  let player1 = "scissors";
+  checkWinner(player1, player2);
+  console.log("player1Turn", player1)
+  console.log(player2)
 }
 
 function player2Turn() {
@@ -29,25 +118,18 @@ function player2Turn() {
 
   switch (randNum) {
     case 1:
-      player2 = "ROCK";
+      player2 = "rock";
       break;
     case 2:
-      player2 = "PAPER";
+      player2 = "paper";
       break;
     case 3:
-      player2 = "SCISSORS";
+      player2 = "scissors";
       break;
   }
 }
 
-function checkWinner() {
-  if (player1 == player2) {
-    return "Draw!";
-  } else if (player2 == "ROCK") {
-    return player1 == "PAPER" ? "You Win!" : "You Lose!";
-  } else if (player2 == "PAPER") {
-    return player1 == "SCISSORS" ? "You Win!" : "You Lose!";
-  } else if (player2 == "SCISSORS") {
-    return player1 == "ROCK" ? "You Win!" : "You Lose!";
-  }
-}
+
+
+
+player2Turn();
